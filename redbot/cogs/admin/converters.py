c3806632cca35f53f62d0108d08a8350bc59eedf
ethanb12007 <73799838+ethanb12007@users.noreply.github.com>
2021-04-19 21:25:43 -0400

    Update converters.py

--- redbot/cogs/admin/converters.py
+++ redbot/cogs/admin/converters.py
@@ -1,9 +1,9 @@
 import discord
 from redbot.core import commands
 from redbot.core.i18n import Translator
+from redbot.core.utils import AsyncIter
 
 _ = Translator("AdminConverters", __file__)
-
 
 class SelfRole(commands.Converter):
     async def convert(self, ctx: commands.Context, arg: str) -> discord.Role:
@@ -11,11 +11,31 @@ class SelfRole(commands.Converter):
         if admin is None:
             raise commands.BadArgument(_("The Admin cog is not loaded."))
 
-        role_converter = commands.RoleConverter()
-        role = await role_converter.convert(ctx, arg)
-
         selfroles = await admin.config.guild(ctx.guild).selfroles()
+        role_converter = commands.RoleConverter()
 
-        if role.id not in selfroles:
-            raise commands.BadArgument(_("The provided role is not a valid selfrole."))
+        pool = set()
+        async for role_id in AsyncIter(selfroles, steps=100):
+            role = ctx.guild.get_role(role_id)
+
+            if role is None:
+                continue
+            if role.name.lower() == arg.lower():
+
+                pool.add(role)
+
+        if not pool:
+            role = await role_converter.convert(ctx, arg)
+            if role.id not in selfroles:
+                raise commands.BadArgument(_("The provided role is not a valid selfrole."))
+        elif len(pool) > 1:
+            raise commands.BadArgument(
+                _(
+                    "This selfrole has more than one capitalization"
+                    " possibilities.  Please inform a moderator."
+                )
+            )
+        else:
+            role = pool.pop()
+
         return role
